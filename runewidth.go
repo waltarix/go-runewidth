0592e45f6f3808b52ddab24315b2c865ca25cd97
waltari <gitting.crew@gmail.com>
2020-05-22 12:49:17 +0900

    custom

--- runewidth.go
+++ runewidth.go
@@ -1,25 +1,14 @@
 package runewidth
 
 import (
-	"os"
-
 	"github.com/rivo/uniseg"
 )
 
-//go:generate go run script/generate.go
+//go:generate make
 
 var (
-	// EastAsianWidth will be set true if the current locale is CJK
-	EastAsianWidth bool
-
-	// StrictEmojiNeutral should be set false if handle broken fonts
-	StrictEmojiNeutral bool = true
-
 	// DefaultCondition is a condition in current locale
-	DefaultCondition = &Condition{
-		EastAsianWidth:     false,
-		StrictEmojiNeutral: true,
-	}
+	DefaultCondition = &Condition{}
 )
 
 func init() {
@@ -27,36 +16,41 @@ func init() {
 }
 
 func handleEnv() {
-	env := os.Getenv("RUNEWIDTH_EASTASIAN")
-	if env == "" {
-		EastAsianWidth = IsEastAsian()
-	} else {
-		EastAsianWidth = env == "1"
-	}
-	// update DefaultCondition
-	if DefaultCondition.EastAsianWidth != EastAsianWidth {
-		DefaultCondition.EastAsianWidth = EastAsianWidth
-		if len(DefaultCondition.combinedLut) > 0 {
-			DefaultCondition.combinedLut = DefaultCondition.combinedLut[:0]
-			CreateLUT()
-		}
+	if len(DefaultCondition.combinedLut) > 0 {
+		DefaultCondition.combinedLut = DefaultCondition.combinedLut[:0]
+		CreateLUT()
 	}
 }
 
 type interval struct {
 	first rune
 	last  rune
+	width int
 }
 
 type table []interval
 
-func inTables(r rune, ts ...table) bool {
-	for _, t := range ts {
-		if inTable(r, t) {
-			return true
+func wcwidth9_width(r rune) int {
+	if r < wcwidth9_table[0].first {
+		return 1
+	}
+
+	bot := 0
+	top := wcwidth9_table_length
+	for top >= bot {
+		mid := (bot + top) >> 1
+
+		switch {
+		case wcwidth9_table[mid].last < r:
+			bot = mid + 1
+		case wcwidth9_table[mid].first > r:
+			top = mid - 1
+		default:
+			return wcwidth9_table[mid].width
 		}
 	}
-	return false
+
+	return 1
 }
 
 func inTable(r rune, t table) bool {
@@ -82,71 +76,34 @@ func inTable(r rune, t table) bool {
 	return false
 }
 
-var private = table{
-	{0x00E000, 0x00F8FF}, {0x0F0000, 0x0FFFFD}, {0x100000, 0x10FFFD},
-}
-
-var nonprint = table{
-	{0x0000, 0x001F}, {0x007F, 0x009F}, {0x00AD, 0x00AD},
-	{0x070F, 0x070F}, {0x180B, 0x180E}, {0x200B, 0x200F},
-	{0x2028, 0x202E}, {0x206A, 0x206F}, {0xD800, 0xDFFF},
-	{0xFEFF, 0xFEFF}, {0xFFF9, 0xFFFB}, {0xFFFE, 0xFFFF},
-}
-
 // Condition have flag EastAsianWidth whether the current locale is CJK or not.
 type Condition struct {
-	combinedLut        []byte
-	EastAsianWidth     bool
-	StrictEmojiNeutral bool
+	combinedLut []byte
 }
 
 // NewCondition return new instance of Condition which is current locale.
 func NewCondition() *Condition {
-	return &Condition{
-		EastAsianWidth:     EastAsianWidth,
-		StrictEmojiNeutral: StrictEmojiNeutral,
-	}
+	return &Condition{}
 }
 
 // RuneWidth returns the number of cells in r.
 // See http://www.unicode.org/reports/tr11/
 func (c *Condition) RuneWidth(r rune) int {
-	if r < 0 || r > 0x10FFFF {
+	if r < 0x20 || r > 0x10FFFF {
 		return 0
 	}
 	if len(c.combinedLut) > 0 {
 		return int(c.combinedLut[r>>1]>>(uint(r&1)*4)) & 3
 	}
 	// optimized version, verified by TestRuneWidthChecksums()
-	if !c.EastAsianWidth {
-		switch {
-		case r < 0x20:
+	switch {
+	case (r >= 0x7F && r <= 0x9F) || r == 0xAD: // nonprint
+		return 0
+	default:
+		if width := wcwidth9_width(r); width < 0 {
 			return 0
-		case (r >= 0x7F && r <= 0x9F) || r == 0xAD: // nonprint
-			return 0
-		case r < 0x300:
-			return 1
-		case inTable(r, narrow):
-			return 1
-		case inTables(r, nonprint, combining):
-			return 0
-		case inTable(r, doublewidth):
-			return 2
-		default:
-			return 1
-		}
-	} else {
-		switch {
-		case inTables(r, nonprint, combining):
-			return 0
-		case inTable(r, narrow):
-			return 1
-		case inTables(r, ambiguous, doublewidth):
-			return 2
-		case !c.StrictEmojiNeutral && inTables(r, ambiguous, emoji, narrow):
-			return 2
-		default:
-			return 1
+		} else {
+			return width
 		}
 	}
 }
@@ -271,14 +228,8 @@ func RuneWidth(r rune) int {
 	return DefaultCondition.RuneWidth(r)
 }
 
-// IsAmbiguousWidth returns whether is ambiguous width or not.
 func IsAmbiguousWidth(r rune) bool {
-	return inTables(r, private, ambiguous)
-}
-
-// IsNeutralWidth returns whether is neutral width or not.
-func IsNeutralWidth(r rune) bool {
-	return inTable(r, neutral)
+	return false
 }
 
 // StringWidth return width as you can see
--- runewidth_test.go
+++ runewidth_test.go
@@ -6,7 +6,6 @@ package runewidth
 import (
 	"crypto/sha256"
 	"fmt"
-	"os"
 	"sort"
 	"testing"
 	"unicode/utf8"
@@ -15,7 +14,6 @@ import (
 var _ sort.Interface = (*table)(nil) // ensure that type "table" does implement sort.Interface
 
 func init() {
-	os.Setenv("RUNEWIDTH_EASTASIAN", "")
 	handleEnv()
 }
 
@@ -39,14 +37,7 @@ type tableInfo struct {
 }
 
 var tables = []tableInfo{
-	{private, "private", 137468, "a4a641206dc8c5de80bd9f03515a54a706a5a4904c7684dc6a33d65c967a51b2"},
-	{nonprint, "nonprint", 2143, "288904683eb225e7c4c0bd3ee481b53e8dace404ec31d443afdbc4d13729fe95"},
-	{combining, "combining", 465, "3cce13deb5e23f9f7327f2b1ef162328285a7dcf277a98302a8f7cdd43971268"},
-	{doublewidth, "doublewidth", 182440, "3d16eda8650dc2c92d6318d32f0b4a74fda5a278db2d4544b1dd65863394823c"},
-	{ambiguous, "ambiguous", 138739, "d05e339a10f296de6547ff3d6c5aee32f627f6555477afebd4a3b7e3cf74c9e3"},
-	{emoji, "emoji", 3535, "9ec17351601d49c535658de8d129c1d0ccda2e620669fc39a2faaee7dedcef6d"},
-	{narrow, "narrow", 111, "fa897699c5e3cd9141c638d539331b0bdd508b874e22996c5e929767d455fc5a"},
-	{neutral, "neutral", 27333, "5455f5e75c307f70b4e9b2384dc5a8bcd91a4c5e2b24b2b185dfad4d860ee5c2"},
+	{wcwidth9_table, "wcwidth9_table", 349453, "1c3441dddc86a1e0c318909c76fc15f4cb939d5c176df5757646852ce1889a74"},
 }
 
 func TestTableChecksums(t *testing.T) {
@@ -68,17 +59,14 @@ func TestTableChecksums(t *testing.T) {
 
 func TestRuneWidthChecksums(t *testing.T) {
 	var testcases = []struct {
-		name           string
-		eastAsianWidth bool
-		wantSHA        string
+		name    string
+		wantSHA string
 	}{
-		{"ea-no", false, "4eb632b105d3b2c800dda9141381d0b8a95250a3a5c7f1a5ca2c4d4daaa85234"},
-		{"ea-yes", true, "c2ddc3bdf42d81d4c23050e21eda46eb639b38b15322d35e8eb6c26f3b83ce92"},
+		{"wcwidth9", "6d02e91cef304759ba416e9a8db9fb4d0e68046292b3203cdea291a093de277b"},
 	}
 
 	for _, testcase := range testcases {
 		c := NewCondition()
-		c.EastAsianWidth = testcase.eastAsianWidth
 		buf := make([]byte, utf8.MaxRune+1)
 		for r := rune(0); r <= utf8.MaxRune; r++ {
 			buf[r] = byte(c.RuneWidth(r))
@@ -104,26 +92,16 @@ func TestRuneWidthChecksums(t *testing.T) {
 
 func TestDefaultLUT(t *testing.T) {
 	var testcases = []struct {
-		name           string
-		eastAsianWidth bool
-		wantSHA        string
+		name    string
+		wantSHA string
 	}{
-		{"ea-no", false, "4eb632b105d3b2c800dda9141381d0b8a95250a3a5c7f1a5ca2c4d4daaa85234"},
-		{"ea-yes", true, "c2ddc3bdf42d81d4c23050e21eda46eb639b38b15322d35e8eb6c26f3b83ce92"},
+		{"wcwidth9", "6d02e91cef304759ba416e9a8db9fb4d0e68046292b3203cdea291a093de277b"},
 	}
-
-	old := os.Getenv("RUNEWIDTH_EASTASIAN")
-	defer os.Setenv("RUNEWIDTH_EASTASIAN", old)
 
 	CreateLUT()
 	for _, testcase := range testcases {
 		c := DefaultCondition
 
-		if testcase.eastAsianWidth {
-			os.Setenv("RUNEWIDTH_EASTASIAN", "1")
-		} else {
-			os.Setenv("RUNEWIDTH_EASTASIAN", "0")
-		}
 		handleEnv()
 
 		buf := make([]byte, utf8.MaxRune+1)
@@ -162,137 +140,66 @@ func isCompact(t *testing.T, ti *tableInfo) bool {
 	return true
 }
 
-func TestSorted(t *testing.T) {
-	for _, ti := range tables {
-		if !sort.IsSorted(&ti.tbl) {
-			t.Errorf("table not sorted: %s", ti.name)
-		}
-		if !isCompact(t, &ti) {
-			t.Errorf("table not compact: %s", ti.name)
-		}
-	}
-}
-
 var runewidthtests = []struct {
-	in     rune
-	out    int
-	eaout  int
-	nseout int
+	in  rune
+	out int
 }{
-	{'世', 2, 2, 2},
-	{'界', 2, 2, 2},
-	{'ｾ', 1, 1, 1},
-	{'ｶ', 1, 1, 1},
-	{'ｲ', 1, 1, 1},
-	{'☆', 1, 2, 2}, // double width in ambiguous
-	{'☺', 1, 1, 2},
-	{'☻', 1, 1, 2},
-	{'♥', 1, 2, 2},
-	{'♦', 1, 1, 2},
-	{'♣', 1, 2, 2},
-	{'♠', 1, 2, 2},
-	{'♂', 1, 2, 2},
-	{'♀', 1, 2, 2},
-	{'♪', 1, 2, 2},
-	{'♫', 1, 1, 2},
-	{'☼', 1, 1, 2},
-	{'↕', 1, 2, 2},
-	{'‼', 1, 1, 2},
-	{'↔', 1, 2, 2},
-	{'\x00', 0, 0, 0},
-	{'\x01', 0, 0, 0},
-	{'\u0300', 0, 0, 0},
-	{'\u2028', 0, 0, 0},
-	{'\u2029', 0, 0, 0},
-	{'a', 1, 1, 1}, // ASCII classified as "na" (narrow)
-	{'⟦', 1, 1, 1}, // non-ASCII classified as "na" (narrow)
-	{'👁', 1, 1, 2},
+	{'世', 2},
+	{'界', 2},
+	{'ｾ', 1},
+	{'ｶ', 1},
+	{'ｲ', 1},
+	{'☆', 2}, // double width in ambiguous
+	{'☺', 1},
+	{'☻', 1},
+	{'♥', 2},
+	{'♦', 1},
+	{'♣', 2},
+	{'♠', 2},
+	{'♂', 2},
+	{'♀', 2},
+	{'♪', 2},
+	{'♫', 1},
+	{'☼', 1},
+	{'↕', 2},
+	{'‼', 1},
+	{'↔', 2},
+	{'\x00', 0},
+	{'\x01', 0},
+	{'\u0300', 0},
+	{'\u2028', 0},
+	{'\u2029', 0},
+	{'a', 1}, // ASCII classified as "na" (narrow)
+	{'⟦', 1}, // non-ASCII classified as "na" (narrow)
+	{'👁', 2},
+	{'', 2},
+	{'﬿', 2},
+	{'\uFD37', 2},
 }
 
 func TestRuneWidth(t *testing.T) {
 	c := NewCondition()
-	c.EastAsianWidth = false
 	for _, tt := range runewidthtests {
 		if out := c.RuneWidth(tt.in); out != tt.out {
-			t.Errorf("RuneWidth(%q) = %d, want %d (EastAsianWidth=false)", tt.in, out, tt.out)
-		}
-	}
-	c.EastAsianWidth = true
-	for _, tt := range runewidthtests {
-		if out := c.RuneWidth(tt.in); out != tt.eaout {
-			t.Errorf("RuneWidth(%q) = %d, want %d (EastAsianWidth=true)", tt.in, out, tt.eaout)
-		}
-	}
-	c.StrictEmojiNeutral = false
-	for _, tt := range runewidthtests {
-		if out := c.RuneWidth(tt.in); out != tt.nseout {
-			t.Errorf("RuneWidth(%q) = %d, want %d (StrictEmojiNeutral=false)", tt.in, out, tt.eaout)
-		}
-	}
-}
-
-var isambiguouswidthtests = []struct {
-	in  rune
-	out bool
-}{
-	{'世', false},
-	{'■', true},
-	{'界', false},
-	{'○', true},
-	{'㈱', false},
-	{'①', true},
-	{'②', true},
-	{'③', true},
-	{'④', true},
-	{'⑤', true},
-	{'⑥', true},
-	{'⑦', true},
-	{'⑧', true},
-	{'⑨', true},
-	{'⑩', true},
-	{'⑪', true},
-	{'⑫', true},
-	{'⑬', true},
-	{'⑭', true},
-	{'⑮', true},
-	{'⑯', true},
-	{'⑰', true},
-	{'⑱', true},
-	{'⑲', true},
-	{'⑳', true},
-	{'☆', true},
-}
-
-func TestIsAmbiguousWidth(t *testing.T) {
-	for _, tt := range isambiguouswidthtests {
-		if out := IsAmbiguousWidth(tt.in); out != tt.out {
-			t.Errorf("IsAmbiguousWidth(%q) = %v, want %v", tt.in, out, tt.out)
+			t.Errorf("RuneWidth(%q) = %d, want %d", tt.in, out, tt.out)
 		}
 	}
 }
 
 var stringwidthtests = []struct {
-	in    string
-	out   int
-	eaout int
+	in  string
+	out int
 }{
-	{"■㈱の世界①", 10, 12},
-	{"スター☆", 7, 8},
-	{"つのだ☆HIRO", 11, 12},
+	{"■㈱の世界①", 11},
+	{"スター☆", 8},
+	{"つのだ☆HIRO", 12},
 }
 
 func TestStringWidth(t *testing.T) {
 	c := NewCondition()
-	c.EastAsianWidth = false
 	for _, tt := range stringwidthtests {
 		if out := c.StringWidth(tt.in); out != tt.out {
 			t.Errorf("StringWidth(%q) = %d, want %d", tt.in, out, tt.out)
-		}
-	}
-	c.EastAsianWidth = true
-	for _, tt := range stringwidthtests {
-		if out := c.StringWidth(tt.in); out != tt.eaout {
-			t.Errorf("StringWidth(%q) = %d, want %d (EA)", tt.in, out, tt.eaout)
 		}
 	}
 }
@@ -380,27 +287,6 @@ func TestTruncateNoNeeded(t *testing.T) {
 	}
 }
 
-var isneutralwidthtests = []struct {
-	in  rune
-	out bool
-}{
-	{'→', false},
-	{'┊', false},
-	{'┈', false},
-	{'～', false},
-	{'└', false},
-	{'⣀', true},
-	{'⣀', true},
-}
-
-func TestIsNeutralWidth(t *testing.T) {
-	for _, tt := range isneutralwidthtests {
-		if out := IsNeutralWidth(tt.in); out != tt.out {
-			t.Errorf("IsNeutralWidth(%q) = %v, want %v", tt.in, out, tt.out)
-		}
-	}
-}
-
 func TestFillLeft(t *testing.T) {
 	s := "あxいうえお"
 	expected := "    あxいうえお"
@@ -438,10 +324,6 @@ func TestFillRightFit(t *testing.T) {
 }
 
 func TestEnv(t *testing.T) {
-	old := os.Getenv("RUNEWIDTH_EASTASIAN")
-	defer os.Setenv("RUNEWIDTH_EASTASIAN", old)
-
-	os.Setenv("RUNEWIDTH_EASTASIAN", "0")
 	handleEnv()
 
 	if w := RuneWidth('│'); w != 1 {
@@ -462,7 +344,7 @@ func TestZeroWidthJoiner(t *testing.T) {
 		{"‍🍳", 2},
 		{"👨‍👨", 2},
 		{"👨‍👨‍👧", 2},
-		{"🏳️‍🌈", 1},
+		{"🏳️‍🌈", 2},
 		{"あ👩‍🍳い", 6},
 		{"あ‍🍳い", 6},
 		{"あ‍い", 4},
